439c1ad8f22cb79de416ddbd0dadec764cf1d369
lazyop <unaskar65@gmail.com>
2025-05-26 00:30:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-// index.js
 const express = require("express");
 const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 
-// Bot token (hardcoded)
 const BOT_TOKEN = "7893322030:AAEjS_TbKslgdl6iVrhDQ-atnufHZ95KkO0";
 const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
-
-// List of admin chat IDs
 const adminChatIds = ['978702643', '6391185835'];
 
 app.post("/api/bot", async (req, res) => {
-  const update = req.body;
-  if (!update.message) return res.sendStatus(200);
+  try {
+    const update = req.body;
+    if (!update.message) return res.sendStatus(200);
 
-  const message = update.message;
-  const fromId = message.from.id;
+    const message = update.message;
+    const fromId = message.from.id;
 
-  // Admin replying to forwarded message
-  if (
-    adminChatIds.includes(fromId.toString()) &&
-    message.reply_to_message &&
-    message.reply_to_message.forward_from
-  ) {
-    const userId = message.reply_to_message.forward_from.id;
+    if (
+      adminChatIds.includes(fromId.toString()) &&
+      message.reply_to_message &&
+      message.reply_to_message.forward_from
+    ) {
+      const userId = message.reply_to_message.forward_from.id;
 
-    if (message.text) {
-      await sendMessage(userId, message.text);
-    } else if (message.photo) {
-      const photo = message.photo[message.photo.length - 1].file_id;
-      const caption = message.caption || "";
-      await sendPhoto(userId, photo, caption);
+      if (message.text) {
+        await sendMessage(userId, message.text);
+      } else if (message.photo) {
+        const photo = message.photo[message.photo.length - 1].file_id;
+        const caption = message.caption || "";
+        await sendPhoto(userId, photo, caption);
+      }
+    } else {
+      const chatId = message.chat.id;
+      const msgId = message.message_id;
+
+      for (const adminId of adminChatIds) {
+        await forwardMessage(adminId, chatId, msgId);
+      }
     }
-  } else {
-    // Forward normal user message to all admins
-    const chatId = message.chat.id;
-    const msgId = message.message_id;
 
-    for (const adminId of adminChatIds) {
-      await forwardMessage(adminId, chatId, msgId);
-    }
+    res.sendStatus(200);
+  } catch (err) {
+    console.error("Error in bot:", err);
+    res.sendStatus(500);
   }
-
-  res.sendStatus(200);
 });
 
 async function sendMessage(chatId, text) {
@@ -70,4 +69,5 @@ async function forwardMessage(toChatId, fromChatId, messageId) {
   });
 }
 
+// Vercel expects the app to be exported (not app.listen)
 module.exports = app;
